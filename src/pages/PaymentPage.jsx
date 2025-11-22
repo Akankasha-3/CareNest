d5c45cd27e2e61112c9919d9e0a4e769e1501217
Akankasha Korrayi <143738635+Akankasha-3@users.noreply.github.com>
2025-11-22 20:10:33 +0530

    Update PaymentPage.jsx

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -29,7 +29,7 @@ const PaymentPage = () => {
   
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar currentPage="" />
+      <Navbar currentPage="payment" />
       
       <div className="flex-grow bg-gray-50 py-12">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
